b05d411e4e36c70db44b6ccd58f1765a6d4e4399
Apoorv Singh <116475419+Apoorv-Singh02@users.noreply.github.com>
2023-05-16 02:22:19 +0530

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVyRBVvubj2N3PxRA1UtUJekSEMPMPXns",
+  authDomain: "liveauction-d1e9d.firebaseapp.com",
+  projectId: "liveauction-d1e9d",
+  storageBucket: "liveauction-d1e9d.appspot.com",
+  messagingSenderId: "338387433812",
+  appId: "1:338387433812:web:2b4f0848f9dd9e8177f0c1",
+  measurementId: "G-JBH07604P6"
+};
+
+const app = initializeApp(firebaseConfig);
