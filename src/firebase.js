510288dc7b702c7841b87b94edc4dca22a317f9d
Apoorv Singh <116475419+Apoorv-Singh02@users.noreply.github.com>
2023-05-17 01:44:19 +0530

    Implemented authentication and redux

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDVyRBVvubj2N3PxRA1UtUJekSEMPMPXns",
@@ -7,7 +9,12 @@ const firebaseConfig = {
   storageBucket: "liveauction-d1e9d.appspot.com",
   messagingSenderId: "338387433812",
   appId: "1:338387433812:web:2b4f0848f9dd9e8177f0c1",
-  measurementId: "G-JBH07604P6"
+  measurementId: "G-JBH07604P6",
+  databaseURL:"https://liveauction-d1e9d-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
+
+export { auth, database }
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import './LoginScreen.css'
+import { login,logout } from '../app/userSlice'
+import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
 
 function LoginScreen() {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  const dispatch = useDispatch()
+
+  const handleSignIn = (e) => {
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log(userCredential)
+    dispatch(login(userCredential.user))
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+    setEmail("")
+    setPassword("")
+  }
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    console.log(userCredential)
+    dispatch(login(userCredential.user))
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+  setEmail("")
+  setPassword("")
+  }
+
   return (
-    <div></div>
+    <div className='full-screen'>
+      <div className='login-card'>
+        <h1 style={{textAlign:'center'}}>Login</h1>
+        <form onSubmit={handleSignIn} >
+          <input value={email} type="email" placeholder='Email Address' className='login-input' onChange={(e)=>{
+            setEmail(e.target.value)
+          }}/>
+          <input value={password} type="password" placeholder='Password' className='login-input' onChange={(e)=>{
+            setPassword(e.target.value)
+          }}/>
+          <button type='submit' className='login-btn'>Sign In</button>
+        </form>
+        <h3>New to this platform, <span className='login-span' onClick={handleSignUp}>Sign Up</span></h3>
+      </div>
+    </div>
   )
 }
 
